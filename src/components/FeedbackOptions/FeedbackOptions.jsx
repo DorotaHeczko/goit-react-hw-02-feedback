e24f25cdad3cac8ab48720d36b6113ae39c14ce7
Dorota Heczko <117163329+DorotaHeczko@users.noreply.github.com>
2023-06-17 07:36:41 +0200

    feedback

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <>
+    {options.map((name, index) => {
+     const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+      return (
+        <div>
+          <button
+            type="button"
+            onClick={onLeaveFeedback}
+            name={name}
+            key={index}
+            className={css.button}
+          >
+            {capitalized}
+          </button>
+        </div>
+      );
+    })}
+  </>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func,
+};
